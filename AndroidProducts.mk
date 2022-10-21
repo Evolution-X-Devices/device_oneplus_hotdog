9d68b6e8a186226d68ebe584ff2a019a2682690b
AnierinB <anierin@evolution-x.org>
2022-10-21 22:27:43 +0000

    hotdog: Initialize Evolution X 7.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,14 @@
 #
 # Copyright (C) 2019 The LineageOS Project
+#               2022 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hotdog.mk
+    $(LOCAL_DIR)/evolution_hotdog.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_hotdog-user \
+    evolution_hotdog-userdebug \
+    evolution_hotdog-eng
